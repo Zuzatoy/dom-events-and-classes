3b85f3f4f795faac19219d84f9dde9081ee08ff7
Elizaveta Serous <zuzatoy@gmail.com>
2018-07-09 21:45:46 +1200

    final commit

--- game.js
+++ game.js
@@ -1,56 +1,71 @@
 // Don't change or delete this line! It waits until the DOM has loaded, then calls 
 // the start function. More info: 
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener('DOMContentLoaded', start);
 
-function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+function start() {
+  bindEventListeners(document.getElementsByClassName('board')[0].children);
 }
 
-function bindEventListeners (dots) {
+function bindEventListeners(dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
-    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('contextmenu', makeGreen);
+    dots[i].addEventListener('click', makeBlue);
     dots[i].addEventListener('dblclick', hide);
   }
 }
 
-function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+function makeGreen(evt) {
+  evt.preventDefault();
+  evt.target.classList.toggle('green');
+  updateCounts();
 }
 
 // CREATE FUNCTION makeBlue HERE
-function makeBlue(evt)
-{
+function makeBlue(evt) {
   evt.target.classList.toggle('blue');
   updateCounts();
 }
 
 // CREATE FUNCTION hide HERE
 
-function hide(evt){
+function hide(evt) {
   evt.target.classList.toggle('invisible');
   updateCounts();
 }
 
-function updateCounts () {
+function updateCounts() {
   var totals = {
     blue: 0,
     green: 0,
-    invisible: 0
+    invisible: 0,
   }
-  
+
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  var dots = document.getElementsByClassName('board')[0].children;
+
+  for (var i=0; i < dots.length; i++) {
+    var isBlue=dots[i].classList.contains('blue');
+    var isGreen=dots[i].classList.contains('green');
+    var isHide=dots[i].classList.contains('invisible');
+
+    if(isBlue) {
+      totals.blue++;
+    }
+    if(isGreen){
+      totals.green++;
+    } else {
+      totals.invisible++;
+    }
+  }
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
 
-function displayTotals (totals) {
+function displayTotals(totals) {
   for (var key in totals) {
     document.getElementById(key + '-total').innerHTML = totals[key]
   }
